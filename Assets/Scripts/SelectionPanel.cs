c892e8252f39a307c347cf73cdd70fa1856d6d4c
Anishchenko Stanislav <anischenko_stas@mail.ru>
2022-07-13 13:52:24 +0500

    Finished

---
+++ Assets/Scripts/SelectionPanel.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class SelectionPanel : MonoBehaviour
+{
+    [SerializeField] private ItemData[] _itemDatas;
+    [SerializeField] private ObjectPlacer _objectPlacer;
+    [SerializeField] private GameObject _itemTemplate;
+    [SerializeField] private Transform _container;
+
+    private void Start()
+    {
+        for (int i = 0; i < _itemDatas.Length; i++)
+            AddItem(_itemDatas[i]);
+    }
+
+    private void AddItem(ItemData itemData)
+    {
+        Instantiate(_itemTemplate, _container).TryGetComponent(out ItemView itemView);
+        itemView.Init(itemData);
+        itemView.ItemSelected += OnItemSelected;
+        itemView.ItemDisabled += OnItemDisabled;
+    }
+
+    private void OnItemSelected(ItemData itemData)
+    {
+        _objectPlacer.SetInstalled(itemData);
+    }
+
+    private void OnItemDisabled(ItemView itemView)
+    {
+        itemView.ItemSelected -= OnItemSelected;
+        itemView.ItemDisabled -= OnItemDisabled;
+    }
+}
